b6027108d60542f9e9f39ed4bbbefb1a72e94465
rfldln <158580261+rfldln@users.noreply.github.com>
2025-03-06 14:13:37 +0800

    Update login-system.py

--- login-system.py
+++ login-system.py
@@ -562,7 +562,7 @@ def main():
         }
         
         voice_conversion_model_options = {
-            "English Conversion Model": "eleven_english_sts_v2"
+            "English Conversion Model": "eleven_english_sts_v2",
             "Multilingual Voice Conversion": "eleven_multilingual_v2"
         }
         
